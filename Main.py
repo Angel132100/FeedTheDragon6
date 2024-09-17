ac6095778ea22ffbeb6c6a7692b61ede801d9147
Angel Parker <parkeang000@warren.k12.in.us>
2024-09-17 09:57:30 -0400

    second commit

--- Main.py
+++ Main.py
@@ -1,24 +1,146 @@
 import pygame
 
-import pygame
-
-#Set display surface
+# Set display surface
 pygame.init()
 
-#Set display surface
+# Set display surface
 WINDOW_WIDTH = 1000
 WINDOW_HEIGHT = 400
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Feed The Dragon")
 
-''' We will write more code here '''
+#Set FPS and clock
+FPS = 60
+clock = pygame.time.Clock()
 
-#The main game loop
+#Set game value:  CONSTANT_NAME, value
+''' 5 CONSTANTS
+PLAYER_STARTING_LIVES, 5
+PLAYER_VELOCITY, 10
+COIN_STARTING_VELOCITY, 10
+COIN_ACCELERATION, 0.5
+BUFFER_DISTANCE, 100
+'''
+PLAYER_STARTING_LIVES = 5
+#YOU do the remaining 4 CONSTANTS
+
+#Set Game Variables:  variable_name
+''' 3 variables
+score, 0
+player_lives, PLAYER_STARTING_LIVES
+coin_velocity, COIN_STARTING_VELOCITY
+'''
+score = 0
+#YOU do the remaining 2 variables
+
+#Set colors
+GREEN = (0, 255, 0)
+DARKGREEN = (10, 50, 10)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+#Set fonts
+font = pygame.font.Font('AttackGraffiti.ttf', 32)
+
+#Set Text for Score
+'''
+variable names:  score_text, score_rect
+render text: "Score: " + str(score)
+antialias: True
+color: GREEN
+background: DARKGREEN
+rect location: topleft = (10, 10)  
+'''
+score_text = font.render("Score: " + str(score), True, GREEN, DARKGREEN)
+score_rect = score_text.get_rect()
+score_rect.topleft = (10, 10)
+
+#Set Text for Title (Similar to Score)
+'''
+variable names:  title_text , title_rect 
+render text: "Feed the Dragon"
+antialias: True
+color: GREEN
+background: WHITE
+rect location: centerx = WINDOW_WIDTH//2
+rect location: y = 10 
+'''
+
+
+#Set Text for Lives (Similar to Score)
+'''
+variable names:  lives_text, lives_rect
+render text: "Lives: " + str(player_lives)
+antialias: True
+color: GREEN
+background: DARKGREEN
+rect location: y = 10  
+'''
+
+
+#Set Text for Game Over (Similar to Score)
+'''
+variable names:  game_over_text , game_over_rect 
+render text: "GAMEOVER"
+antialias: True
+color: GREEN
+background: DARKGREEN
+rect location: center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2) 
+'''
+
+
+
+#Set Text for Continue (Similar to Score)
+'''
+variable names:  continue_text, continue_rect  
+render text: "Press any key to play again"
+antialias: True
+color: GREEN
+background: DARKGREEN
+rect location: center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2 + 32)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# The main game loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-pygame.quit()
+    # Fill the display
+    display_surface.fill(BLACK)
 
+    display_surface.blit(score_text, score_rect)
+    ''' YOU DO:  blit title_text and title_rect'''
+    ''' YOU DO:  blit lives_text and lives_rect'''
+
+    # Update display and tick the clock
+    pygame.display.update()
+    clock.tick(FPS)
+
+pygame.quit()
